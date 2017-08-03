dbe34e0a9e42902356622d5f45d577bdadca8358
yuxp <1039354317@qq.com>
2017-08-03 18:39:08 +0800

    gulp基础环境通过

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,23 @@
 var gulp = require('gulp')
-var uglify = require('gulp-uglify')
-var rename = require('gulp-rename')
+var $ = require('gulp-load-plugins')()
 
 gulp.task('minijs', function() {
 	gulp.src('src/js/**/*.js')
-		.pipe(uglify())
-		.pipe(gulp.dest('build/js'))
+		.pipe($.uglify())
+		.pipe(gulp.dest('dist/js'))
+})
+gulp.task('watch1', function() {
+    gulp.watch('src/less/*.less', function() {
+        gulp.src('src/less/*.less')
+            .pipe($.less())
+            .pipe(gulp.dest('dist/css'))
+    })
 })
 
+gulp.task('less', function() {
+    gulp.src('src/less/*.less')
+        .pipe($.less())
+        .pipe(gulp.dest('dist/css'))
+})
+gulp.task('build', ['minijs', ])
+gulp.task('dev', ['watch1'])
